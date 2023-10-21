20cbf798a75038e6002603e60d4c6123ad2d026b
promise96319 <1248975357@qq.com>
2023-10-21 21:31:33 +0800

    feat: update

--- src/contentScripts/views/CctvVideoDownloader.vue
+++ src/contentScripts/views/CctvVideoDownloader.vue
@@ -4,6 +4,7 @@ import { sendMessage } from 'webext-bridge/content-script'
 import { downloadUrl, log, transformM3u8oMp4 } from '~/util'
 
 const loading = ref<boolean>(false)
+const isCctvWebsite = globalThis.location?.href.startsWith('https://v.cctv.com')
 
 const download = async () => {
   const requestUrl = await sendMessage<string>('get-cctv-video-url', {})
@@ -27,13 +28,13 @@ const download = async () => {
 </script>
 
 <template>
-  <div class="fixed right-0 bottom-0 m-5 z-100 flex items-end font-sans select-none leading-1em">
+  <div v-if="isCctvWebsite" class="fixed right-0 bottom-0 m-5 z-100 flex items-end font-sans select-none leading-1em">
     <button
-      class="flex w-10 h-10 rounded-full shadow cursor-pointer border-none" bg="teal-600 hover:teal-700"
-      @click="download"
+      class="flex items-center justify-center w-64px h-64px rounded-full shadow cursor-pointer border-none"
+      bg="teal-600 hover:teal-700" @click="download"
     >
-      <ant-design-loading-outlined v-if="loading" class="block m-auto text-white text-lg animate-rotate" />
-      <ant-design-download-outlined v-else class="block m-auto text-white text-lg" />
+      <ant-design-loading-outlined v-if="loading" class="block m-auto text-white text-32px animate-rotate" />
+      <ant-design-download-outlined v-else class="block m-auto text-white text-32px" />
     </button>
   </div>
 </template>
